e5a8eb39081bf64292c8b3379ab800de14c5abe1
rajarshideb100 <rajarshideb100@gmail.com>
2026-08-16 20:11:47 +0530

    Remove temporary deployment test files

--- tourism_project/deployment/test_sklearn18/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import sklearn
-
-st.title("scikit-learn 1.8.0 Deployment Test")
-st.write("Streamlit started successfully.")
-st.write("Pandas version:", pd.__version__)
-st.write("scikit-learn version:", sklearn.__version__)
