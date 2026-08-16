cab0a4a09586c18828b3114d2299f1c1e4ec1de5
rajarshideb100 <rajarshideb100@gmail.com>
2026-08-16 20:06:27 +0530

    Add isolated scikit-learn only deployment test

---
+++ tourism_project/deployment/test_sklearn_only/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import sklearn
+
+st.title("scikit-learn Only Deployment Test")
+st.write("Streamlit started successfully.")
+st.write("scikit-learn version:", sklearn.__version__)
